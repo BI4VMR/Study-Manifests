f422c5334ac704cfefe8b68be8236dde78caf7c8
BI4VMR <bi4vmr@outlook.com>
2024-07-12 00:13:26 +0800

    D61636D6-F6CC-4BDE-A77C-A13577961FC1

---
+++ script/git-push-private-lan.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+function push(){
+    if [ -d $1 ];then
+        echo -e "\n开始处理项目【$1】..."
+        cd $1
+        if [[ -n $(git status -s) ]]
+        then
+            msg=$(uuidgen | awk "{print toupper(\$0)}"); git add .; git commit -m "$msg";
+        else
+            echo -e "\n项目【$1】无变化，无需提交。"
+        fi
+        git push private-lan master;
+        cd ..
+    else
+        echo -e "\n项目【$1】不存在，已跳过！"
+    fi
+}
+
+push KnowledgeBase
+push Study-C
+push Study-CPP
+push Study-Java
+push Study-Kotlin
+push Study-Android
+push Study-DesignPattern
+push Study-Network
+push Study-Manifests
